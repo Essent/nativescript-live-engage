842bd60231d4d7d1624bf7e529d7813beba97710
Natasa Vukovic <n.vukovic@levi9.com>
2019-10-22 20:22:22 +0200

    ABD-1242 - Added generated typing for dependency okhttp jar

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,14 +1,18 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./platforms/android/typings/okhttp/android.d.ts" />
 
-/// <reference path="./platforms/ios/typings/objc!LPAMS.d.ts" />
-/// <reference path="./platforms/ios/typings/objc!LPInfra.d.ts" />
-/// <reference path="./platforms/ios/typings/objc!LPMessagingSDK.d.ts" />
-/// <reference path="./platforms/ios/typings/objc!LPMonitoring.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!LPAMS.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!LPInfra.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!LPMessagingSDK.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!LPMonitoring.d.ts" />
 
-/// <reference path="./platforms/android/typings/lp_messaging_sdk/android.d.ts" />
-/// <reference path="./platforms/android/typings/infra/android.d.ts" />
-/// <reference path="./platforms/android/typings/messaging/android.d.ts" />
-/// <reference path="./platforms/android/typings/messaging_ui/android.d.ts" />
-/// <reference path="./platforms/android/typings/monitoring/android.d.ts" />
-/// <reference path="./platforms/android/typings/ui/android.d.ts" />
+
+/// <reference path="./platforms/android/typings/lp_messaging_sdk/android.d.ts" />
+/// <reference path="./platforms/android/typings/infra/android.d.ts" />
+/// <reference path="./platforms/android/typings/messaging/android.d.ts" />
+/// <reference path="./platforms/android/typings/messaging_ui/android.d.ts" />
+/// <reference path="./platforms/android/typings/monitoring/android.d.ts" />
+/// <reference path="./platforms/android/typings/ui/android.d.ts" />
+
+
